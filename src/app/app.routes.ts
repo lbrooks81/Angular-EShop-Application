039962eaeeb661bef28fa77c6c7083d3212c9881
lbrooks81 <143453730+lbrooks81@users.noreply.github.com>
2025-01-11 15:54:42 -0500

    Day One

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {ProductListComponent} from './products/product-list/product-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'list', redirectTo: '', pathMatch: 'full'}, //* If the user types in "list" in full, it will redirect to the home page
+  { path: '', component: ProductListComponent} //* When the path is empty, display the product list component
+
+];
